923ca74244072307400763c77c3700e986345a6b
rafhael josino <rafhael_josino1@hotmail.com>
2026-08-26 21:44:04 -0300

    routes update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import IronWarriors from '../components/IronWarriors';
-import DeathGuard from '../components/DeathGuard';
+// import IronWarriors from '../components/IronWarriors';
+// import DeathGuard from '../components/DeathGuard';
+import ChartContainer from '../components/ChartContainer';
 
 function routes() {
   return <Routes>
     <Route path='/*' element={<Home />} />
-    <Route path='/Iron Warriors/*' element={<IronWarriors />} />
-    <Route path='/Death Guard/*' element={<DeathGuard />} />
-    {/* <Route path='/MiddleEarth/*' element={<ChartContainer />} /> */}
+    {/* <Route path='/Iron Warriors/*' element={<IronWarriors />} />
+    <Route path='/Death Guard/*' element={<DeathGuard />} /> */}
+    <Route path='/MiddleEarth/*' element={<ChartContainer />} />
   </Routes>
 }
 
