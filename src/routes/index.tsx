bbd27c9aa4dcd0802567ca0553394f27166e7d87
rafhael josino <rafhael_josino1@hotmail.com>
2026-08-26 22:03:09 -0300

    old world wound chart test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+// import Home from '../pages/Home';
 // import IronWarriors from '../components/IronWarriors';
 // import DeathGuard from '../components/DeathGuard';
 import ChartContainer from '../components/ChartContainer';
